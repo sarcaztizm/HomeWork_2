cf0ed403374e4b5d23a04ad151aea6a42fd33bda
Eric <ericismag@outlook.com>
2022-09-07 01:37:10 +0300

    Задание №2 Найти вторую цифру числа

---
+++ Task_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write ("Введите число ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number / 100 < 9)
+{
+    int Result = Number / 10;
+    Result = Result % 10;
+    Console.Write ($"Вторая цифра числа {Number} = {Result}");
+}
+else
+{
+    Console.Write ("Введено неправильное число, повторите попытку");
+}
