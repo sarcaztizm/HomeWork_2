1e72e56f1c12663ee9e75e5b4d2b275c3b2edb57
Eric <ericismag@outlook.com>
2022-09-07 02:02:45 +0300

    Задание №3 Выходные дни

---
+++ Task3_Weekends/Program.cs
@@ -0,0 +1,22 @@
+﻿// Принять на вход цифру обозначающую день неднли
+// и проверить является ли этот день выходным
+
+Console.Write ("Введите число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number < 8 && Number > 5)
+{
+    Console.WriteLine("Выходной");
+    if (Number == 6)    
+    {
+        Console.Write("Суббота");
+    }
+    else
+    {
+        Console.Write("Воскресенье");
+    }
+}
+else
+{
+    Console.Write("Введено неправильное число или день недели не выходной");
+}
