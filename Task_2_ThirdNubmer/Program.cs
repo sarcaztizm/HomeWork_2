a8fd08a5d0c6acd7bb07a10b28e0deddad329bfc
Eric <ericismag@outlook.com>
2022-09-07 01:50:56 +0300

    Задание №2 Найти третью цифру числа

---
+++ Task_2_ThirdNubmer/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывести третью цийру слева числа
+// и сообщить, что третьей цифры нет
+
+Console.Write ("Введите число ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number / 100 > 0)
+{
+    int Result = Number % 1000;
+    Result = Result /100;
+    Console.Write($"Третья цифра числа {Number} = {Result}");
+}
+else
+{
+    Console.Write("Введено неправильное число, повторите попытку");
+}
